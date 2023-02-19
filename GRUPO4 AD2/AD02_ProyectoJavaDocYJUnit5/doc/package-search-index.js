d3a87169df0b16296707f7452dca7eb6843e58ad
Gabriela.Prieto.Herrera <gabiprieto7@gmail.com>
2023-02-19 23:59:38 +0000

    add jugador y soldado

---
+++ GRUPO4 AD2/AD02_ProyectoJavaDocYJUnit5/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"junit5.programa"},{"l":"programa"}];updateSearchResults();
---
+++ GRUPO4 AD2/AD02_ProyectoJavaDocYJUnit5/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"programa","l":"Jugador"},{"p":"programa","l":"Soldado"}];updateSearchResults();

52497db747a285c03681f49ca128faed0b0b7f13
Maria Antònia Fernández Mateu <mafernandezmateu@gmail.com>
2023-02-19 10:02:34 +0100

    JavaDoc generado

--- GRUPO4_AD2/src/Jugador.java
+++ GRUPO4_AD2/src/Jugador.java
@@ -1,27 +1,59 @@
-
+/**
+ * 
+ * @author Marian
+ *
+ */
 public class Jugador {
 	private int dorsal;
 	private int numeroTarjetasAmarillas;
 	private int numeroTarjetasRojas;
 	
+	/**
+	 * Consulta el numero del dorsal
+	 * @return el numero del dorsal
+	 */
 	public int getDorsal() {
 		return dorsal;
 	}
+	/**
+	 * Define el valor del dorsal
+	 * @param dorsal numero
+	 */
 	public void setDorsal(int dorsal) {
 		this.dorsal = dorsal;
 	}
+	/**
+	 * Consulta el numero de tarjetas amarillas
+	 * @return numero de tarjetas amarillas
+	 */
 	public int getNumeroTarjetasAmarillas() {
 		return numeroTarjetasAmarillas;
 	}
+	/**
+	 * Define el numero de tarjetas amarillas
+	 * @param numeroTarjetasAmarillas 1 o 2.
+	 */
 	public void setNumeroTarjetasAmarillas(int numeroTarjetasAmarillas) {
 		this.numeroTarjetasAmarillas = numeroTarjetasAmarillas;
 	}
+	/**
+	 * Evalua el numero de tarjetas rojas
+	 * @return numero de tarjetas rojas
+	 */
 	public int getNumeroTarjetasRojas() {
 		return numeroTarjetasRojas;
 	}
+	/**
+	 * Define el numero de tarjetas rojas.
+	 * @param numeroTarjetasRojas numero
+	 */
 	public void setNumeroTarjetasRojas(int numeroTarjetasRojas) {
 		this.numeroTarjetasRojas = numeroTarjetasRojas;
 	}
+	/**
+	 * Configura el valor del dorsal que tienen que estar comprendidos entre 1 y 30. En otro, se configura como -1.
+	 * @param dorsal numero entre 1 y 30
+	 */
 	public void ponerDorsal(int dorsal){
 		if(dorsal >= 1&&dorsal <= 30) {
 			this.dorsal = dorsal;
@@ -29,7 +61,10 @@ public class Jugador {
 			this.dorsal = -1;
 		}
 	}
-	
+	/**
+	 * Evalua si es expulsado. Si tiene 2 tarjtas amarillas o una roja esta expulsado.
+	 * @return true si esta expulsado y false en cualquier otro caso.
+	 */
 	public boolean estaExpulsado() {
 		boolean expulsado = false;
 		if(numeroTarjetasAmarillas==2) { expulsado= true;}
--- GRUPO4_AD2/src/Soldado.java
+++ GRUPO4_AD2/src/Soldado.java
@@ -1,31 +1,63 @@
+/**
+ * Representa un soldado en combate.
+ * @author Marian
+ */
 public class Soldado {
 	private boolean estaMuerto;
 	private int numeroBalas;
 	
+	/**
+	 * Inicializa un soldado con cierto numero de balas.
+	 * @param numeroBalas municion inicial
+	 */
 	public Soldado (int numeroBalas) {
 		this.estaMuerto = false;
 		this.numeroBalas = numeroBalas; 
 	}
 	
+	/**
+	 * Evalua si el soldado esta muerto
+	 * @return true si esta muerto o false si esta vivo.
+	 */
 	public boolean isEstaMuerto() {
 		return estaMuerto;
 	}
+	/**
+	 * clasifica si esta vivo el soldado.
+	 * @param estaMuerto true o false.
+	 */
 	public void setEstaMuerto(boolean estaMuerto) {
 		this.estaMuerto = estaMuerto;
 	}
+	/**
+	 * Indica las balas que le quedan en el arma.
+	 * @return numero de balas.
+	 */
 	public int getNumeroBalas() {
 		return numeroBalas;
 	}
+	
+	/**
+	 * 	Le indica al soldado la disponibilidad de numero de balas.
+	 * @param numeroBalas numero de balas
+	 */
 	public void setNumeroBalas(int numeroBalas) {
 		this.numeroBalas = numeroBalas;
 	}
-	
+	/**
+	 * Indica si tiene material para disparar.
+	 * @return true en caso de tener mas de 0 balas o false si tiene 0.
+	 */ 
 	public boolean puedeDisparar() {
 		if (this.numeroBalas > 0) {
 			return true;
 		}
 		return false;	
 	}
+	/**
+	 * Descuenta una bala y mata al soldadol.
+	 * @param sol, soldado a quien dispara.
+	 */
 	public void disparar(Soldado sol) {
 		this.numeroBalas--;
 		sol.estaMuerto=true;	
